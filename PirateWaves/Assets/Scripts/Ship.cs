53ef535af55b54afe75c782d3bdfe92a07d9de67
tobnyl <tobnyl@gmail.com>
2017-01-20 22:34:47 +0100

    Slerp for base cannon!!

--- PirateWaves/Assets/Scripts/Ship.cs
+++ PirateWaves/Assets/Scripts/Ship.cs
@@ -7,9 +7,11 @@ public class Ship : MonoBehaviour
     public int Index;
     public float ForwardForce = 1f;
     public float RotationForce = 1f;
-
+    
     [Space(10f)]
     public GameObject BaseCanon;
+    public float BaseCannonRotationSpeedY = 10f;
+    public float BaseCannonSlerpSpeedY = 1f;
 
     private Rigidbody _rigidbody;
     private float _currentBaseCanonAngleY;
@@ -44,13 +46,12 @@ public class Ship : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        _currentBaseCanonAngleY += AxisRight.x*10f;
+        _currentBaseCanonAngleY += AxisRight.x * BaseCannonRotationSpeedY;
 
-        var newRotationY = Quaternion.AngleAxis(_currentBaseCanonAngleY, Vector3.up);
+        var newRotationY = Quaternion.AngleAxis(_currentBaseCanonAngleY, BaseCanon.transform.up);
+        	    
+        BaseCanon.transform.rotation = Quaternion.Slerp(BaseCanon.transform.rotation, newRotationY, Time.deltaTime * BaseCannonSlerpSpeedY);        
 
-	    BaseCanon.transform.rotation = newRotationY;
-            
-         //   Quaternion.Slerp(transform.rotation, newRotationY,
-	        //Time.deltaTime*10f);        
-	}
+
+    }
 }
