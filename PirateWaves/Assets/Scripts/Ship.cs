94e94fd3b895e0c3f0c61f1c89d980008405cc32
tobnyl <tobnyl@gmail.com>
2017-01-20 18:50:42 +0100

    Save

---
+++ PirateWaves/Assets/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public int Index;
+
+    private Vector3 AxisLeft
+    {
+        get { return new Vector3(Input.GetAxis("Horizontal" + Index), 0, Input.GetAxis("Vertical" + Index)); }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+	        Debug.Log("Fire!");
+	    }
+
+		Debug.Log(AxisLeft);
+	}
+}
---
+++ PirateWaves/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    #region Fields/Properties
+
+    public GameObject Target;
+    public float DistanceFromTarget = 5.0f;
+    public float OffsetY;
+
+    private Vector3 _offset;
+    private Vector3 _offsetY;
+
+    #endregion
+
+    #region Events
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position = Target.transform.position - Target.transform.forward * DistanceFromTarget;
+
+        _offset = transform.position - Target.transform.position;
+        _offsetY = new Vector3(0, OffsetY, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        transform.position = _offset + Target.transform.position + _offsetY;
+    }
+
+    #endregion
+}
