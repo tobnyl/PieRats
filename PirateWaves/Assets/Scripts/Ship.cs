c49f68781e5cd8d4593336b6770558240fa2076b
tobnyl <tobnyl@gmail.com>
2017-01-21 11:00:15 +0100

    Canon rotation

--- PirateWaves/Assets/Scripts/Ship.cs
+++ PirateWaves/Assets/Scripts/Ship.cs
@@ -9,14 +9,20 @@ public class Ship : MonoBehaviour
     public int Index;
     public float ForwardForce = 1f;
     public float RotationForce = 1f;
-    
-    [Space(10f)]
+        
+    [Header("Base Canon")]
     public GameObject BaseCanon;
     public float BaseCannonRotationSpeedY = 10f;
     public float BaseCannonSlerpSpeedY = 1f;
+    [Header("Canon")]
+    public GameObject Canon;
+    public float CannonRotationSpeedX = 10f;
+    public float CannonSlerpSpeedX = 1f;
+    public Vector2 CanonAngleLimit;
 
     private Rigidbody _rigidbody;
     private float _currentBaseCanonAngleY;
+    private float _currentCanonAngleX;
 
     private Vector3 AxisLeft
     {
@@ -45,11 +51,31 @@ public class Ship : MonoBehaviour
 
 	void Update ()
 	{
+        RotateBaseCannon();
+	    RotateCannon();
+	}
+
+    #endregion
+    #region Methods
+
+    private void RotateBaseCannon()
+    {
         _currentBaseCanonAngleY += AxisRight.x * BaseCannonRotationSpeedY;
 
         var newRotationY = Quaternion.AngleAxis(_currentBaseCanonAngleY + transform.rotation.eulerAngles.y, BaseCanon.transform.up);
-        	    
-        BaseCanon.transform.rotation = Quaternion.Slerp(BaseCanon.transform.rotation, newRotationY, Time.deltaTime * BaseCannonSlerpSpeedY);        
+
+        BaseCanon.transform.rotation = Quaternion.Slerp(BaseCanon.transform.rotation, newRotationY, Time.deltaTime * BaseCannonSlerpSpeedY);
+    }
+
+    private void RotateCannon()
+    {
+        _currentCanonAngleX += AxisRight.z * CannonRotationSpeedX;
+
+        _currentCanonAngleX = Mathf.Clamp(_currentCanonAngleX, CanonAngleLimit.x, CanonAngleLimit.y);
+
+        Debug.Log(_currentCanonAngleX);
+        var newRotationX = Quaternion.AngleAxis(_currentCanonAngleX, BaseCanon.transform.right);
+        Canon.transform.rotation = Quaternion.Slerp(Canon.transform.rotation, newRotationX, Time.deltaTime*CannonSlerpSpeedX);
     }
 
     #endregion
